c63e8061d5153dc8a1fc7862f4d1b5126026a5f8
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-23 21:11:34 +0200

    1. Utilities 2. Iterators 3. Transforms 4. Itertools

---
+++ Built-In Functions/itertools.py
@@ -0,0 +1,45 @@
+# advanced iteration functions in the itertools package
+
+import itertools
+
+def testFunction(x):
+    return x < 40
+
+def main():
+    # TODO: cycle iterator can be used to cycle over a collection
+    seq1 = ["Joe", "John", "Mike"]
+    cycle1 = itertools.cycle(seq1)
+    print(next(cycle1))
+    print(next(cycle1))
+    print(next(cycle1))
+    print(next(cycle1))
+    print(next(cycle1))
+
+    # TODO: use count to create simple counter
+    count1 = itertools.count(100, 10)
+    print(next(count1))
+    print(next(count1))
+    print(next(count1))
+    print(next(count1))
+    print(next(count1))
+
+
+    # TODO: accumulate creates an iterator that accumulates values
+    vals = [10,20,30,40,50,40,30]
+    acc = itertools.accumulate(vals)
+    print(list(acc))
+
+    # TODO: use chain to conenct sequences togheter
+    x = itertools.chain("ABCD", "1234")
+    print(list(x))
+
+    # TODO:  dropwhile and takewhile will return values until
+    # a certain condition is met that stops them
+    print(list(itertools.dropwhile(testFunction, vals)))
+    print(list(itertools.takewhile(testFunction, vals)))
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Built-In Functions/utilities.py
@@ -0,0 +1,23 @@
+# built-in utility functions
+
+def main():
+    # use any() and all() to test sequences for boolean values
+    list1 = [1, 2, 3, 0, 5, 6]
+
+    # TODO: any will return true if any of the sequence values are true
+    print(any(list1))
+
+    # TODO: all will return true only if all values are true
+    print(all(list1))
+
+    # TODO: min and max will return minimum and maximum values in a sequence
+    print("min: ", min(list1))
+    print("max: ", max(list1))
+
+    # TODO: Use sum() to sum up all of the values in a sequence
+    print("sum: ", sum(list1))
+
+
+if __name__ == "__main__":
+    main()
+
