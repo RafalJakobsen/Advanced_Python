64913010d0174652a8940f3612436036ad44de03
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-27 15:52:11 +0200

    1. Advanced collections overview 2. Using namedtuple 3. defualtdict 4. Counters 5. OrderedDict 6. Deque objects

---
+++ Advanced Collections/namedtup.py
@@ -0,0 +1,20 @@
+# Demonstrate the usage of namdtuple objects
+
+import collections
+
+
+def main():
+    # TODO: create a Point namedtuple
+    Point = collections.namedtuple("Point", "x y")
+    p1 = Point(10, 20)
+    p2 = Point(30, 40)
+    print(p1, p2)
+    print(p1.x, p2.y)
+
+    # TODO: use _replace to create a new instance
+    p1 = p1._replace(x=100)
+    print(p1)
+
+
+if __name__ == "__main__":
+    main()
