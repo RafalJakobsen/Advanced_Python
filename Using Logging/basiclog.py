772a55305c7e822b3753ec2b9f98a3c7487281b6
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-10-30 06:57:24 +0100

    1. Basic Logging 2. Custom logging

---
+++ Using Logging/basiclog.py
@@ -0,0 +1,23 @@
+# demonstrate the logging api in Python
+
+# TODO: use the built-in logging module
+import logging
+
+def main():
+    # TODO: Use basicConfig to configure logging
+    logging.basicConfig(level=logging.DEBUG,
+                        filename="output.log",
+                        filemode = "w")
+
+    # Try out each of the log levels
+    logging.debug("This is a debug message")
+    logging.info("This is a info message")
+    logging.warning("This is a warning message")
+    logging.error("This is a erro message")
+
+    # TODO: Output formatted strings to the log
+    logging.info("Here`s a {} variable and an int:".format("string", 10))
+
+if __name__ == "__main__":
+    main()
+
